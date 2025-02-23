1d454b7861278288da83a7b8fa4a8c20426a61b4
engsr6982 <pwxg286704@163.com>
2025-02-23 13:55:14 +0800

    feat(v8): add move and copy delete to v8 scope classes

--- src/v8/V8Scope.h
+++ src/v8/V8Scope.h
@@ -12,7 +12,7 @@
 namespace jse {
 
 
-class EnterV8Scope {
+class EnterV8Scope final {
     v8::Locker         locker_;
     v8::Isolate::Scope isolateScope_;
     v8::HandleScope    handleScope_;
@@ -24,20 +24,35 @@ public:
       isolateScope_(isolate),
       handleScope_(isolate),
       contextScope_(context) {}
+
+    EnterV8Scope(const EnterV8Scope&)            = delete;
+    EnterV8Scope& operator=(const EnterV8Scope&) = delete;
+    EnterV8Scope(EnterV8Scope&&)                 = delete;
+    EnterV8Scope& operator=(EnterV8Scope&&)      = delete;
 };
 
-class ExitV8Scope {
+class ExitV8Scope final {
     v8::Unlocker locker_;
 
 public:
     explicit ExitV8Scope(v8::Isolate* isolate) : locker_(isolate) {}
+
+    ExitV8Scope(const ExitV8Scope&)            = delete;
+    ExitV8Scope& operator=(const ExitV8Scope&) = delete;
+    ExitV8Scope(ExitV8Scope&&)                 = delete;
+    ExitV8Scope& operator=(ExitV8Scope&&)      = delete;
 };
 
-class HandleV8Scope {
+class HandleV8Scope final {
     v8::EscapableHandleScope handleScope_;
 
 public:
     explicit HandleV8Scope(v8::Isolate* isolate) : handleScope_(isolate) {}
+
+    HandleV8Scope(const HandleV8Scope&)            = delete;
+    HandleV8Scope& operator=(const HandleV8Scope&) = delete;
+    HandleV8Scope(HandleV8Scope&&)                 = delete;
+    HandleV8Scope& operator=(HandleV8Scope&&)      = delete;
 
     template <typename T>
     v8::Local<v8::Value> Escape(v8::Local<T> value) {
@@ -46,7 +61,7 @@ public:
 };
 
 
-inline void HandleV8Exception(v8::Isolate* isolate, v8::Local<v8::Context> ctx, v8::TryCatch& vtry) {
+inline void CheckV8Exception(v8::Isolate* isolate, v8::Local<v8::Context> ctx, v8::TryCatch& vtry) {
     if (vtry.HasCaught()) {
         v8::String::Utf8Value exception(isolate, vtry.Exception());
         v8::String::Utf8Value stack(isolate, vtry.StackTrace(ctx).ToLocalChecked());
